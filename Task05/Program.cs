595f8a24c00713c5ed0193ff62401a7b3bdf5d88
ray-seven <ray-seven@yandex.ru>
2017-11-13 22:48:16 +0400

    Lab 01 Task 05

---
+++ Task05/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task05
+{
+    class Program
+    {
+        const int firstDivider = 3;
+        const int secondDivider = 5;
+
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            FindMultipleOfDivider(numbers, firstDivider);
+            FindMultipleOfDivider(numbers, secondDivider);
+            numbers = numbers.Distinct().ToList<int>();
+            Console.WriteLine("Sum of all multiples: {0}", CalculateSum(numbers));
+            Console.Write("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        private static int CalculateSum(List<int> numbers)
+        {
+            int result = 0;
+            foreach (int x in numbers)
+            {
+                result += x;
+            }
+            return result;
+        }
+
+        private static void FindMultipleOfDivider(List<int> numbers, int divider)
+        {
+            int number = 0;
+            while (number < 1000 - divider)
+            {
+                number += divider;
+                numbers.Add(number);
+            }
+        }
+    }
+}
