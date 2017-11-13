add0672ce00ea04540b4d71cfb0af14c29359fb5
ray-seven <ray-seven@yandex.ru>
2017-11-14 00:58:16 +0400

    Lab 01 Task 10 Done

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task08
+namespace Task10
 {
     class Program
     {
@@ -15,57 +15,54 @@ namespace Task08
 
         private static void MainLogic()
         {
-            int[,,] array = new int[10,10,10];
+            int[,] array = new int[3, 3];
             GenerateArray(array);
-            ArrayOfHopelessness(array);
-            ShowResults(array);
+            int sumOfHonestPositionsElements = SumOfHonestPositionsElements(array);
+            ShowResults(sumOfHonestPositionsElements, array);
             TryAgain();
         }
 
-        private static void ArrayOfHopelessness(int[,,] array)
+        private static int SumOfHonestPositionsElements(int[,] array)
         {
+            int result = 0;
             for (int i = array.GetLowerBound(0); i <= array.GetUpperBound(0); i++)
             {
                 for (int j = array.GetLowerBound(1); j <= array.GetUpperBound(1); j++)
                 {
-                    for (int k = array.GetLowerBound(2); k <= array.GetUpperBound(2); k++)
+                    if ((i + j) % 2 == 0)
                     {
-                        if (array[i, j, k] >=0) { array[i, j, k] = 0; }
+                        result += array[i, j];
                     }
                 }
             }
+            return result;
         }
 
-        private static void ShowResults(int[,,] array)
+        private static void ShowResults(int sum, int[,] array)
         {
             Console.WriteLine("Array:");
             for (int i = array.GetLowerBound(0); i <= array.GetUpperBound(0); i++)
             {
                 for (int j = array.GetLowerBound(1); j <= array.GetUpperBound(1); j++)
                 {
-                    for (int k = array.GetLowerBound(2); k <= array.GetUpperBound(2); k++)
-                    {
-                        Console.Write("{0,4} ", array[i, j, k]);
-                    }
-                    Console.WriteLine("");
+                    Console.Write("{0,4} ", array[i, j]);                    
                 }
-                Console.WriteLine(Environment.NewLine);
+                Console.WriteLine("");
             }
+            Console.WriteLine("");
+            Console.WriteLine("Sum of honest positions elements is {0}", sum);
         }
 
-        private static void GenerateArray(int[,,] array)
+        private static void GenerateArray(int[,] array)
         {
             Random rnd = new Random();
             for (int i = array.GetLowerBound(0); i <= array.GetUpperBound(0); i++)
             {
                 for (int j = array.GetLowerBound(1); j <= array.GetUpperBound(1); j++)
                 {
-                    for (int k = array.GetLowerBound(2); k <= array.GetUpperBound(2); k++)
-                    {
-                        array[i,j,k] = rnd.Next(-100, 100);
-                    }
+                    array[i, j] = rnd.Next(-100, 100);
                 }
-            } 
+            }
         }
 
         private static void TryAgain()
