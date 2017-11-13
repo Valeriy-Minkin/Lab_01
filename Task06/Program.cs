662f3c96f0c8337d438c031d9842563b8c56a0c1
ray-seven <ray-seven@yandex.ru>
2017-11-13 23:37:19 +0400

    Lab 01 Task 06 Done

---
+++ Task06/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task06
+{
+    class Program
+    {
+        [Flags]
+        enum FontStyle:byte
+        {
+            None = 0,
+            Bold = 1,
+            Italic = 2,
+            Underline =4,
+        }
+
+        static void Main(string[] args)
+        {
+            FontStyle style = FontStyle.None;
+            Console.ForegroundColor = ConsoleColor.Green;
+            ShowStyles(style);
+            MainLogic(ref style);
+        }
+
+        private static void MainLogic(ref FontStyle style)
+        {
+            SelectStyleType(ReadStyle(), ref style);
+        }
+
+        private static void SelectStyleType(char v, ref FontStyle style)
+        {
+            switch (v)
+            {
+                case '1':
+                    ChangeStyle(ref style, FontStyle.Bold);
+                    break;
+                case '2':
+                    ChangeStyle(ref style, FontStyle.Italic);
+                    break;
+                case '3':
+                    ChangeStyle(ref style, FontStyle.Underline);
+                    break;
+            }
+        }
+
+        private static char ReadStyle()
+        {
+            Console.WriteLine("Введите:");
+            Console.WriteLine("       1: bold");
+            Console.WriteLine("       2: italic");
+            Console.WriteLine("       3: underline");
+            Console.WriteLine("Любую другую клавишу для выхода");
+            return Console.ReadKey().KeyChar;
+        }
+
+        private static void ChangeStyle(ref FontStyle style, FontStyle newStyle)
+        {
+            style = style ^ newStyle;
+            ShowStyles(style);
+            MainLogic(ref style);
+        }
+
+        private static void ShowStyles(FontStyle style)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Параметры надписи: {0}", style);
+        }
+    }
+}
